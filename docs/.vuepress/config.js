85cb21837c313e1552d7af78244614fce22fb7c8
mingjiu <boss_think@163.com>
2025-03-24 23:16:59 +0800

    feat: add 博客首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  base: '/ise-blog/'
-}
+  base: "/ise-blog/",
+  title: "前端探索者：在代码海洋中破浪前行",
+  description:
+    "记录我在前端领域的深度探索，从攻克复杂技术难题，到钻研最新技术，分享成长路上的点滴心得与实战经验。",
+  themeConfig: {
+    logo: "/logo.svg",
+    nav: [{ text: "首页", link: "/home/" }],
+  },
+};
